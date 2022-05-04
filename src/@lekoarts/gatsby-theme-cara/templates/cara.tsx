aaecfcde822dffbbaa9fd55943d5b99392c98555
Duncan <twoontheriver@gmail.com>
2022-05-04 17:16:45 +0100

    minor adjustment

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -88,8 +88,8 @@ function useWindowWidth() {
       case width <= 400:
         breakpoints.pages = 8;
 
-        breakpoints.projectsScroll = 1.8;
-        breakpoints.projectsOffset = 1.85;
+        breakpoints.projectsScroll = 1.75;
+        breakpoints.projectsOffset = 1.8;
         breakpoints.projectsFactor = 2.4;
 
         breakpoints.techOffset = 4.4;
